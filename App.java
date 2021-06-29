ef2429badaf5b575c5244c96b10cf4052bd88395
koloversh <ImmortalNickolas@yandex.ru>
2021-06-29 15:39:34 +0300

    Begin.

---
+++ App.java
@@ -0,0 +1,35 @@
+package com.example.astrophoto;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private NasaService nasaService;
+    private RoscosmosService roscosmosService;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        nasaService = new NasaService();
+
+        roscosmosService = new RoscosmosService();
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true).build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new LruMemoryCache(20 * 1024 * 1024))
+                .memoryCacheSize(20 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
+    }
+
+    public NasaService getNasaService() {
+        return nasaService;
+    }
+
+    public RoscosmosService getRoscosmosService() {
+        return roscosmosService;
+    }
+}
---
+++ PhotoListActivity.java
@@ -0,0 +1,4 @@
+package com.example.astrophoto;
+
+public class PhotoListActivity {
+}
